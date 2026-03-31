5c4c9b31d72f6419bdca363d1532ccc04dfbad88
Tanishq <tanishq162005@gmail.com>
2026-03-31 22:53:13 +0530

    feat(reflection): add reflection engine and exports

---
+++ src/u_core/reflection/__init__.py
@@ -0,0 +1,9 @@
+"""Reflection engine exports."""
+
+from .engine import PlannerExecutionOutcome, ReflectionApplyResult, apply_execution_reflection
+
+__all__ = [
+    "PlannerExecutionOutcome",
+    "ReflectionApplyResult",
+    "apply_execution_reflection",
+]
